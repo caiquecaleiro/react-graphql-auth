5d5cb2ad8bdb478b6018b772f86279d15c3fb366
Caique Caleiro <caiquecaleiro@gmail.com>
2017-04-24 16:19:28 +1200

    Add project structure

---
+++ server/schema/mutations.js
@@ -0,0 +1,33 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    singup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        AuthService.signup({ email, password, req });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
+    }
+  } 
+});
+
+module.exports = mutation;
